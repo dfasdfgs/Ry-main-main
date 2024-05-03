36ef136779f5344c65d203786ee2605a3fe52eee
KMS <oinkjwpea@gmail.com>
2024-05-03 16:17:02 +0900

    ㅠㅜㅠ 완성

---
+++ Assets/2_Scripts/sdf.cs
@@ -0,0 +1,13 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class sdf : MonoBehaviour
+{
+
+        void Update()
+        {
+            if (Input.GetKey("escape"))
+                Application.Quit();
+
+        }
+}
